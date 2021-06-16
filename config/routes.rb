56d568c4b0d0150d4079f9f4b911b75ea80fe79c
Josh <josh.handon@gmail.com>
2021-06-16 15:36:29 -0400

    commit tree 25ca23d3b9a6a6f791939ad0be9c4eec93fecc88

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :coupons, only: [:index, :show, :new, :create]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #   get 'coupons/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
